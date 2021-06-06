c9c5b1c496bd5371d17877178ff23bf1153ab526
mambru <ecastro82@gmail.com>
2021-06-06 13:10:30 -0500

    working on assignment requirements

--- mambru82-portfolio/src/App.js
+++ mambru82-portfolio/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import './assets/css/style.css';
+import React, {useState} from 'react';
+import './assets/css/index.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Work from './components/Work';
 import Contact from './components/Contact'
 
 function App() {
+  const [categories] = useState([
+      {
+          name: "about",
+          description: "a short description of me"
+      },
+      { name: "work", description: "works in progress"},
+      { name: "contact", description: "contact info"}
+  ]) 
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        ></Nav>
       <main>
         <About></About>
         <Work></Work>
--- mambru82-portfolio/src/components/Nav/index.js
+++ mambru82-portfolio/src/components/Nav/index.js
@@ -1,40 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers"
 import personalPhoto from '../../assets/images/CASTRO-ECHEVERRY.JPG'
 
 
-function Nav() {
-    const categories = [
-        {
-            name: "about",
-            description: "a short description of me"
-        },
-        { name: "work", description: "works in progress"},
-        { name: "contact", description: "contact info"}
-    ]
+function Nav(props) {
 
-    function categorySelected() {
-
-    }
+    const {
+        categories=[],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name)
+    }, [currentCategory]);
 
     return (
-        <header>
-            <img src={personalPhoto}></img>
+       <header className="flex-row px-1">
+            <img src={personalPhoto} alt="portrait of Eduardo Castro"></img>
             <h1>
-        <a href="./index.html">Eduardo Castro</a>
-      </h1>
+                 <a data-testid="link" href="./index.html">Eduardo Castro</a>
+            </h1>
       <nav>
-       <ul>
-   
-        <li>
-          <a href="#about-me">About Me</a>
-        </li>
-        {categories.map((category) => (
-        <li className="mx-1" key={category.name} onClick={categorySelected}>
-          
-            {category.name}
-          
-        </li> ))}
-       </ul>
+        <ul className="flex-row">
+            <li className="mx-2">
+                <a href="#about-me">About Me</a>
+            </li>
+            {categories.map((category) => (
+            <li  className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'}`} 
+                key={category.name} 
+                onClick={()=>setCurrentCategory(category)} >
+            
+                {capitalizeFirstLetter(category.name)}
+            
+            </li> ))}
+        </ul>
       </nav>
         </header>
     )
